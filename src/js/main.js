2d452bb9cc2d752f7034031e97252c3771f0f496
Leo-Marinhoo <leo.ccodes@gmail.com>
2024-08-30 15:25:00 -0300

    responsive layout

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,10 @@
-console.log('teste');
+document.addEventListener('DOMContentLoaded', ()=>{
+    const birthday = new Date("Feb 23, 2025 20:00:00");
+    const time_stamp = (birthday.getTime())/1000;
+
+    new FlipDown(time_stamp)
+    .start()
+    .ifEnded(()=>{
+        console.log('Event has ended!');
+    })
+})
